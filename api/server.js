66b357f4ffeb9443d9066ebd0dadba507022b9c6
Ante <ante.adamovic@asynclabs.co>
2019-11-29 14:54:16 +0100

    Update rebase

--- api/server.js
+++ api/server.js
@@ -1,38 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mysql = require('mysql');
+// const getData = require('./database');
 
 app.use(cors());
-
-const db = mysql.createConnection({
-    host: "localhost",
-    port: 33006,
-    database: "workshop",
-    user: "root",
-    password: "root"
-  });
-
-db.connect(err => {
-    if (err) {
-        console.log('Failed to connect to database!', err);
-        return process.exit(1);
-    }
-
-    console.log('Database connection succesful!');
-});
-
-const getData = () => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM todos', (err, result) => {
-            if (err) {
-               reject(err);
-            }
-        
-            resolve(result);
-        });
-    });
-}
 
 app.get('/mock', (req, res) => {
     res.send([
